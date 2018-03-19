bfe0d712aec55520a86ffaba9a5f8a73cf184335
Camel Léonce Djoulako <24195550+cameldjoulako@users.noreply.github.com>
2018-03-19 21:54:54 +0100

    Update homeework.js, solve exercise

--- Lesson7-JS-IV/homework/homework.js
+++ Lesson7-JS-IV/homework/homework.js
@@ -5,61 +5,113 @@ function makeCat(name, age) {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+  const Cat = {
+    age: 5,
+    meow: function(){
+      return 'Meow!';
+    },
+  };
+  return Cat;
 }
 
 function addProperty(object, property) {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object.name = property;
+  return object;
 }
 
 function invokeMethod(object, method) {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  object[method];
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  objectValue = mysteryNumberObject.mysteryNumber;
+  product = objectValue * 5;
+  return product;
 }
 
 function deleteProperty(object, property) {
   // remove the property from the object
   // return the object
+  delete object.property;
+  return object;
 }
 
 function newUser(name, email, password) {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+  const user = {
+    uname: name,
+    uemail: email,
+    upassword: password,
+  };
+  return user;  
 }
 
 function hasEmail(user) {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  let compt = 0;
+  for(let key in user){
+    if(key == 'email'){
+     compt++;
+    }
+  }
+  if(compt>0){
+    return true;
+
+  }else{
+    return false;
+  }
 }
 
 function hasProperty(object, property) {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  let cpt = 0;
+  for(let key in object){
+    if (object[key] == property){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
 }
 
 function verifyPassword(user, password) {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
-  // otherwise return false
+  // otherwise return false 
+  if(user.password == password){
+    return true;
+  } else{
+    return false;
+  }
 }
 
 function updatePassword(user, newPassword) {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+  user.password = newPassword;
+  return user;
+  
 }
 
 function addFriend(user, newFriend) {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
+  user.friends.push(newFriend);
+  return user;
 }
 
 function setUsersToPremium(users) {
@@ -67,6 +119,9 @@ function setUsersToPremium(users) {
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+  for(let i=0; i<users.length; i++){
+    users[i].isPremium = true;
+  }
 }
 
 function sumUserPostLikes(user) {
@@ -75,6 +130,12 @@ function sumUserPostLikes(user) {
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
+  let som = 0;
+  let nb = user.posts.length;
+  for(let i=0; i<nb; i++){
+    som += user.posts[i].likes;
+  }
+  return som;
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
@@ -86,6 +147,11 @@ function addCalculateDiscountPriceMethod(storeItem) {
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
+  storeItem.calculateDiscountPrice = function(){
+    let discount = storeItem.price * discountPercentage;
+    let discountPrice = storeItem.price - discount;
+  }
+  return storeItem;
 }
 
 // Do not modify code below this line.
